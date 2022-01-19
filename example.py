498002e69a3aac45a973aab8ab6015009ba4c8e0
lars <lars23@ymail.com>
2022-01-19 16:31:39 +0100

    cleanup2

---
+++ example.py
@@ -0,0 +1,79 @@
+import torch
+import pickle
+
+from torch import nn
+from tayloranalysis import TaylorAnalysis
+
+# load data
+
+data = pickle.load(open("data/data.pickle", "rb"), encoding="latin-1")
+x_train = torch.tensor(data['x_train'], dtype=torch.float)
+y_train = torch.tensor(data['y_train'], dtype=torch.float)
+
+# initialize nomral pytorch model
+
+class Mlp(nn.Module):
+    
+    def __init__(self, input_neurons, hidden_neurons, 
+                output_neurons, hiddenlayers):
+        
+        nn.Module.__init__(self)
+
+        # mlp layers
+        self.mlplayers = nn.ModuleList([nn.Linear(input_neurons,
+                hidden_neurons)])
+        self.mlplayers.extend([nn.Linear(hidden_neurons, 
+                                        hidden_neurons) for i in range(hiddenlayers + 1)])
+        self.mlplayers.append(nn.Linear(hidden_neurons, output_neurons))
+
+    def forward(self, x):
+        # input shape: (batch, features)
+        for mlplayer in self.mlplayers[:-1]:
+            x = mlplayer(x)
+            x = torch.tanh(x) 
+        
+        # new x: (batch, 1)
+        x = self.mlplayers[-1](x)
+        x = x.squeeze(-1)  # new x: (batch)
+        x = torch.sigmoid(x)
+        return x
+
+model = Mlp(2, 100, 1, 2)
+
+# wrap model
+
+model = TaylorAnalysis(model)
+
+optim = torch.optim.Adam(model.parameters(), lr=0.001)
+crit = nn.BCELoss()
+
+device=torch.device(0)
+x_train=x_train.to(device)
+y_train=y_train.to(device)
+model.to(device)
+
+x_train.requires_grad = True
+for epoch in range(200):
+    optim.zero_grad()
+    pred = model(x_train)
+    loss = crit(pred, y_train)
+    loss.backward()
+    optim.step()
+    print('Epoch {}: Loss: {:.3f}'.format(epoch+1, loss))
+    
+    # save current taylorcoefficients
+    
+    model.tc_checkpoint(x_train, names=['x1', 'x2'], order=3)
+
+# load test data
+
+x_test = torch.tensor(data['x_test'], dtype=torch.float).to(device)
+
+# plot taylorcoefficients after training
+
+model.plot_tc(data=x_test, names=['x1', 'x2'], order=3)
+
+# plot saved checkpoints
+
+model.plt_checkpoints()
+
