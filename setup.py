ed341e33c4f808f6fce47baf00c5bec850420df9
Artur Monsch <artur.monsch@student.kit.edu>
2022-08-10 16:35:13 +0200

    Switching to setup.py from master

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
-#!/usr/bin/env python
-
+import os
 from setuptools import setup, find_packages
 
-long_description='''Pytorch implementation of the Paper "Identifying the relevant dependencies of the neural network response on characteristics of the input space" (S. Wunsch, R. Friese, R. Wolf, G. Quast). As in the paper explained, the method computes the averaged taylorcoefficients of a taylored model function. These coefficients are noted as . This is the optimal method to identify not only first order feature importance, but also higher order importance (i.e. the importance of combined features). This module can be applied to each differentiable pytorch model with a scalar output value.
-'''
+package_dir = os.path.dirname(os.path.abspath(__file__))
+requirements_file = os.path.join(package_dir, "requirements.txt")
+with open(requirements_file, "r") as rf:
+    install_requires = [req.strip() for req in rf.readlines() if req.strip() and not req.startswith("#")]
 
-setup(name='tayloranalysis',
-      version='0.0.1',
-      description='Taylorcoefficient Analysis for pytorch models',
-      long_description=long_description,
-      author='Lars Sowa, Artur Monsch',
-      author_email='lars.sowa@kit.edu, artur.monsch@student.kit.edu',
-      url='https://github.com/lsowa/tayloranalysis',
-      license='',
-      install_requires=[],
-      download_url='https://github.com/lsowa/tayloranalysis/releases',
-      packages=find_packages()
-     )
+setup(
+    name="Tayloranalysis",
+    version="0.0.1",
+    author="Lars Sowa, Artur Monsch",
+    url="https://github.com/lsowa/tayloranalysis",
+    packages=find_packages(),
+    description='Taylorcoefficient Analysis for pytorch models',
+    long_description='Pytorch implementation of the Paper "Identifying the relevant dependencies of the neural network response on characteristics of the input space" (S. Wunsch, R. Friese, R. Wolf, G. Quast). As in the paper explained, the method computes the averaged taylorcoefficients of a taylored model function. These coefficients are noted as . This is the optimal method to identify not only first order feature importance, but also higher order importance (i.e. the importance of combined features). This module can be applied to each differentiable pytorch model with a scalar output value.',
+    install_requires=install_requires,
+)
