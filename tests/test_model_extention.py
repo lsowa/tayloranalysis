60e45bc2afc63d7647016ca43365e5daeae56867
lars <lars23@ymail.com>
2024-05-03 12:36:21 +0200

    add testing

---
+++ tests/test_model_extention.py
@@ -0,0 +1,106 @@
+import unittest
+import torch
+import itertools
+
+import tayloranalysis as ta
+
+from torch import nn
+
+
+class Mlp(nn.Module):
+    def __init__(self, input_neurons, hidden_neurons, output_neurons, hidden_layers):
+        nn.Module.__init__(self)
+
+        # mlp layers
+        self.linear1 = nn.Linear(input_neurons, output_neurons)
+        self.linear2 = nn.Linear(output_neurons, output_neurons)
+        # self.linear1.weight.data.fill_(0.1)
+        # self.linear1.bias.data.fill_(0.1)
+
+    def forward(self, x):
+        x = self.linear1(x)
+        x = torch.tanh(x)
+        x = self.linear2(x)
+        x = torch.tanh(x)
+        return x
+
+mlp_specs = {
+    "input_neurons": 5,
+    "hidden_neurons": 10,
+    "hidden_layers": 2,
+    "output_neurons": 3,
+}
+
+
+x_data = torch.rand(10, 5)
+
+class TestBaseClass:
+    def test_forward(self):
+        y = self.model(x_data)
+        yshape = list(y.shape)
+        targetshape = [x_data.shape[0], mlp_specs["output_neurons"]]
+        self.assertEqual(yshape, targetshape)
+
+    def test_node_compatibilities(self):
+        # test if all-node results fit singe single-node and max-node results
+        combinations = []
+        for nth_order in [1,2,3]:
+            combinations += [
+                i for i in itertools.permutations(range(mlp_specs["output_neurons"]), nth_order)
+            ]
+        for index in combinations:
+            for eval_max_node_only in [False, True]:
+                node_outputs = []
+                for node in range(mlp_specs["output_neurons"]):
+                    # get singe node results
+                    tc = self.model.get_tc(
+                        x_data=x_data,
+                        index_list=[index],
+                        node=node,
+                        eval_max_node_only=eval_max_node_only,
+                    )
+                    tc = tc[index]
+                    node_outputs.append(tc)
+                # get all-node results
+                tc = self.model.get_tc(
+                    x_data=x_data,
+                    index_list=[index],
+                    node=None,
+                    eval_max_node_only=eval_max_node_only,
+                )
+                tc = tc[index]
+                # sums should be equal
+                node_outputs = torch.stack(node_outputs, dim=-1).sum(dim=-1)
+                with unittest.TestCase().subTest(
+                    index=index, eval_max_node_only=eval_max_node_only
+                ):
+                    # check if tensors are close
+                    is_close = torch.testing.assert_close(tc, node_outputs) == None
+                    self.assertTrue(is_close)
+
+
+class TestClassExtention(TestBaseClass, unittest.TestCase):
+    def setUp(self):
+        # check inheritance method
+        WrappedModel = ta.extend_model(Mlp)
+        self.model = WrappedModel(**mlp_specs)
+
+
+class TestInstanceExtention(TestBaseClass, unittest.TestCase):
+    def setUp(self):
+        # check adding methods to object
+        model = Mlp(**mlp_specs)
+        self.model = ta.extend_model(model)
+
+
+def test_suite():
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(TestClassExtention))
+    suite.addTest(unittest.makeSuite(TestInstanceExtention))
+    return suite
+
+
+if __name__ == "__main__":
+    runner = unittest.TextTestRunner()
+    suite = test_suite()
+    runner.run(suite)
---
+++ tests/test_tc_computation.py
@@ -0,0 +1,58 @@
+import torch
+import unittest
+import itertools
+import tayloranalysis as ta
+
+
+class Polynom(torch.nn.Module):
+    def __init__(self):
+        torch.nn.Module.__init__(self)
+
+    def forward(self, point):
+        y = point[:, 1]
+        x = point[:, 0]
+        return x * y + x * y**2 + y**3
+
+
+class TestTCComputation(unittest.TestCase):
+    def setUp(self):
+        # setup model
+        WrappedModel = ta.extend_model(Polynom)
+        self.model = WrappedModel()
+
+        # setup feature combinations and expected results
+        self.solution_dict = {
+            (0,): 6.0,
+            (1,): 27.0,
+            (0, 0): 0.0,
+            (0, 1): 5.0,
+            (1, 0): 5.0,
+            (1, 1): 9.0,
+            (0, 0, 0): 0.0,
+            (0, 0, 1): 0.0,
+            (0, 1, 0): 0.0,
+            (0, 1, 1): 1.0,
+            (1, 0, 0): 0.0,
+            (1, 0, 1): 1.0,
+            (1, 1, 0): 1.0,
+            (1, 1, 1): 1.0,
+        }
+        # setup data to evaluate
+        self.point = torch.tensor([[3, 2]]).float()
+
+    def test_coefficients(self):
+        for combination in self.solution_dict.keys():
+            # compute TC
+            tc = self.model.get_tc(
+                x_data=self.point,
+                index_list=[combination],
+            )
+            tc = tc[combination].item()  # as float
+            with self.subTest(combination=combination):
+                # compare result to expected value
+                self.assertAlmostEqual(tc, self.solution_dict[combination])
+
+
+if __name__ == "__main__":
+    # run TestTCComputation
+    unittest.main()
